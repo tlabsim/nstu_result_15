b208443bd30871b565bae1cf855f67d6bdd4ea62
Mashpy Says <contact@mashpy.me>
2016-01-05 14:28:48 +0000

    create comit

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'nsturesult',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'Tawhid#2134',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
